5e284b196843d29888d086afc2fd91b5510002e2
Christo Radev <christo.radev@gmail.com>
2025-01-07 23:52:41 +0200

    Add files via upload

---
+++ pdfs/AllFiles.php
@@ -0,0 +1,32 @@
+<?php
+  function sizeFilter( $bytes ) {
+      $label = array( 'B', 'KB', 'MB', 'GB', 'TB', 'PB' );
+      for( $i = 0; $bytes >= 1024 && $i < ( count( $label ) -1 ); $bytes /= 1024, $i++ );
+      return( round( $bytes, 2 ) . " " . $label[$i] );
+  }
+  if ($handle = opendir('.')) {
+    $files=array();
+    while (false !== ($file = readdir($handle))) {
+      if ($file != "." && $file != "..") {
+        $files[] = $file;
+      }
+    }
+    closedir($handle);
+  }
+  natsort($files); //sort
+  foreach($files as $file) {
+        $date = date(filemtime($file));
+        $filelist .= '<li><a href="'.$file.'">'.$file.'</a></li>';
+        $sizelist .= '<li>'.sizeFilter(filesize($file)).'</li>';
+        $timelist .= '<li>'.date('M d Y H:i', $date).'</li>';
+        clearstatcache();
+  }
+?>
+<h1>List of files:</h1>
+<table><tr><pre>
+<td><ul style="list-style-type:none"><li><?php echo $filelist; ?></li><ul></td>
+<td><ul style="list-style-type:none; text-align: right;"><li><?php echo $sizelist; ?></li><ul></td>
+<td><ul style="list-style-type:none"><li><?php echo $timelist; ?></li><ul></td>
+</pre></tr></table>
+
+
